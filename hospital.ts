bd3e8d336ff4d35615168a2faea81b4aad4c5dfc
filipe-kazakevicius <filipe.kazakevicius@etec.sp.gov.br>
2020-03-06 18:41:50 +0000

    implementa

--- hospital.ts
+++ hospital.ts
@@ -1,7 +1,39 @@
 namespace Hospital{
     export class Hospital{
         private nome: string;
-        private enfermeiros: 
+        private enfermeiros: Array <Enfermeiro> = [];
+        private medicos: Array <Medicos> = [];
+        private pacientes: Array <Pacientes> = [];
+
+        getnome (){
+            return this.nome;
+        }
+
+        setnome(nome:string){
+            this.nome = nome;
+        }
+        
+        addEnfermeiros (enfermeiros:Enfermeiros):void{
+            this.enfermeiros.push(enfermeiros);
+        }
+
+        getEnfermeiros(){
+            return this.enfermeiros;
+        }
+
+        addMedicos (medicos:Medicos):void{
+            this.medicos.push(medicos);
+        }
+        getmedicos(){
+            return this.medicos;
+        }
+
+        addPacientes(pacientes:Pacientes):void{
+            this.pacientes.push(pacientes);
+        }
+        getpacientes(){
+            return this.pacientes;
+        }
 
     }
 
---
+++ implementa.ts
@@ -0,0 +1,8 @@
+namespace pessoa{
+
+    let h = new hospital ("Theme Hospital")
+
+    document.getElementById("nome").textContent = h.getNome();
+    document.getElementById("enfermeiro").textContent = h.getEnfermeiros();
+    
+}
